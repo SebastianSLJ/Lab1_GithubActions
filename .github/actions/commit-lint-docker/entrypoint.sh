590208e88b1e176abf1822a29b971119b9c24101
Sebastian L <jleguizamons@unal.edu.co>
2026-09-02 19:14:33 -0500

    fix: entrypoint

--- .github/actions/commit-lint-docker/entrypoint.sh
+++ .github/actions/commit-lint-docker/entrypoint.sh
@@ -6,7 +6,7 @@ REGEX="^(feat|fix|chore|docs): .*"
 echo "Validating commit message: '$COMMIT_MSG'"
 
 if [[ "$COMMIT_MSG" =~ $REGEX ]]; then
-  echo "Commit message valid"
+  echo "Commit message valid!"
   exit 0
 else
   echo "The commit message doesn´t comply with the conventional commits formats"
